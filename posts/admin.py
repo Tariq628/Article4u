e5689734ee0c7f21ee429661a5068f640e5341d3
tariq628 <tariqkaleri628@gmail.com>
2023-06-28 18:35:24 +0500

    Removed cache

--- posts/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Post
-from django_summernote.admin import SummernoteModelAdmin
-
-
-@admin.register(Post)
-class addPostAdmin(SummernoteModelAdmin):
-    list_display = ["id", "title", "category"]
