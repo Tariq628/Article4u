20028d4c66c59f2a00fefbb513ffd4a70b017a06
tariq628 <tariqkaleri628@gmail.com>
2023-06-28 18:10:50 +0500

    Refactoring

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,6 @@
 from django.db import models
-from datetime import datetime
-# Create your models here.
+
+
 class addPost(models.Model):
     postId = models.AutoField(primary_key=True)
     title = models.CharField(max_length=100)
@@ -8,7 +8,9 @@ class addPost(models.Model):
     image = models.ImageField(upload_to="blog/images", default="")
     content = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
+
     class Meta:
         get_latest_by = "postId"
+
     def __str__(self):
         return self.title
--- blog/urls.py
+++ blog/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path
 from . import views
 from .views import Template, JsonTemplate
+
 urlpatterns = [
     # API to post a comment
     path("", views.index, name="index"),
     path("write-post/", views.writePost, name="writePost"),
     path("randomfacts/", Template.as_view(), name="randomFacts"),
-    path("randomfacts/<int:num_posts>/", JsonTemplate.as_view(), name="jsonRandomFacts"),
+    path(
+        "randomfacts/<int:num_posts>/", JsonTemplate.as_view(), name="jsonRandomFacts"
+    ),
     path("technology/", Template.as_view(), name="Technology"),
     path("technology/<int:num_posts>/", JsonTemplate.as_view(), name="jsonTechnology"),
     path("sports/", Template.as_view(), name="Sports"),
@@ -16,5 +19,5 @@ urlpatterns = [
     path("templateview/<int:myid>/", views.TemplateView, name="templateView"),
     path("signup/", views.signUp, name="signup"),
     path("login/", views.userLogin, name="login"),
-    path("logout/", views.Logout, name="logout")
+    path("logout/", views.Logout, name="logout"),
 ]
