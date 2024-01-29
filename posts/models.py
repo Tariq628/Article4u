8ad8e77f3a6f0d7cab5ab04e39d9dab7fc02b245
tariq628 <tariqkaleri628@gmail.com>
2024-01-29 05:29:05 +0500

    Posts not showing in specific categories

--- posts/models.py
+++ posts/models.py
@@ -1,12 +1,26 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 
 class Post(models.Model):
+    RANDOM_FACTS = 'random_facts'
+    TECHNOLOGY = 'technology'
+    SPORTS = 'sports'
+    POETS = 'poets'
+
+    CATEGORY_CHOICES = [
+        (RANDOM_FACTS, 'Random Facts'),
+        (TECHNOLOGY, 'Technology'),
+        (SPORTS, 'Sports'),
+        (POETS, 'Poets'),
+    ]
+
     title = models.CharField(max_length=100)
-    category = models.CharField(max_length=60)
-    image = models.ImageField(upload_to="blog/images", default="")
+    category = models.CharField(max_length=60, choices=CATEGORY_CHOICES)
+    image = models.ImageField(upload_to="blog/images")
     content = models.TextField()
-    date = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
 
     class Meta:
         get_latest_by = "id"
