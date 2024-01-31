65e39afb9dcd470c1ab18ddfa3c8617aabeaffbd
tariq628 <tariqkaleri628@gmail.com>
2024-01-31 22:13:21 +0500

    Added comments model

--- posts/models.py
+++ posts/models.py
@@ -27,3 +27,13 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Comment by {self.author.username} on {self.post.title}"
